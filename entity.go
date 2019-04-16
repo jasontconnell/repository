c7353bffe0714bb175912fd929640b89aba2fced
Jason Connell <jconnell@delphicdigital.com>
2019-04-15 21:36:34 -0400

    Add method to get the Query object instead of auto populating the list

--- entity.go
+++ entity.go
@@ -1,6 +1,9 @@
 package repository
 
-import "github.com/globalsign/mgo/bson"
+import (
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+)
 
 // Entity is a database object
 type Entity interface {
@@ -15,6 +18,11 @@ func (repo *Repository) GetEntityList(list []interface{}) []Entity {
 		entities = append(entities, v.(Entity))
 	}
 	return entities
+}
+
+// GetQuery - get the query object back to do more things against it like sort, limit, etc
+func (repo *Repository) GetQuery(collection string, filter bson.M) *mgo.Query {
+	return repo.OpenCollection(collection).Find(filter)
 }
 
 // GetList returns a filtered list
